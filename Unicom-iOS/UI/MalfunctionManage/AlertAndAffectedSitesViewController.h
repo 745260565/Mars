229ad64e3705d33377919549abca985303a91dcf
jiamai <jiamai@jiamaideMac-mini-cheng.local>
2015-12-15 16:27:39 +0800

    master version2 AlertAndAffectedSites

---
+++ Unicom-iOS/UI/MalfunctionManage/AlertAndAffectedSitesViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlertAndAffectedSitesViewController.h
+//  Unicom-iOS
+//
+//  Created by jiamai on 15/12/15.
+//  Copyright © 2015年 Runsdata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertAndAffectedSitesViewController : UIViewController
+
+@end
