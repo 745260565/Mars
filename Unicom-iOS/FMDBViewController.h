360c9c11860c0cb52493eded58cd6eeac9eafdb4
jiamai <jiamai@jiamaideMac-mini-cheng.local>
2015-12-15 18:16:31 +0800

    Unicom-iOS/FMDBViewController.h

---
+++ Unicom-iOS/FMDBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMDBViewController.h
+//  Unicom-iOS
+//
+//  Created by jiamai on 15/12/15.
+//  Copyright © 2015年 Runsdata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMDBViewController : UIViewController
+
+@end
---
+++ Unicom-iOS/FMDBViewController.m
@@ -0,0 +1,47 @@
+//
+//  FMDBViewController.m
+//  Unicom-iOS
+//
+//  Created by jiamai on 15/12/15.
+//  Copyright © 2015年 Runsdata. All rights reserved.
+//
+
+#import "FMDBViewController.h"
+#import "FMDB.h"
+
+@interface FMDBViewController ()
+
+@end
+
+@implementation FMDBViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+- (IBAction)add:(id)sender {
+    
+}
+- (IBAction)delete:(id)sender {
+}
+- (IBAction)change:(id)sender {
+}
+- (IBAction)search:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
